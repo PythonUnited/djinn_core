e335eccc4b4a740d99f2992773db8b0c9083b272
Duco Dokter <dokter@w20e.com>
2013-09-30 09:54:08 +0200

    release 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_core',
-      version="1.0.0",
+      version="1.0.1",
       description='Djinn Intranet core',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
