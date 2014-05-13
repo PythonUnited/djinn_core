d7684dbab06cbd2b37d110b6b9658a71f198b652
Duco Dokter <dokter@w20e.com>
2014-05-13 22:09:19 +0200

    Added html trunc utility

--- djinn_core/utils.py
+++ djinn_core/utils.py
@@ -1,3 +1,4 @@
+from HTMLParser import HTMLParser
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -75,3 +76,66 @@ def get_object_by_ctype_id(ctype_id, _id, app_label=None):
     ctype = ContentType.objects.get(app_label=app_label, model=ctype_id)
 
     return ctype.get_object_for_this_type(id=_id)
+
+
+
+
+class HTMLTruncate(HTMLParser):
+
+    """ Truncating class for html fragments """
+
+    def __init__(self, limit):
+
+        HTMLParser.__init__(self)
+
+        self.limit = limit
+        self.done = False
+        self.count = 0
+        self.truncated = []
+        self.singles = ["br", "img", "hr"]
+        self.queue = []
+
+    def _flatten_attrs(self, attrs):
+
+        if attrs:
+            return " " + " ".join(
+                ['%s="%s"' % (name, val) for name, val in attrs])
+        else:
+            return ""
+
+    def handle_starttag(self, tag, attrs):
+        if not self.done:
+
+            if tag in self.singles:
+                single_marker = "/"
+            else:
+                single_marker = ""
+
+            self.queue.append(tag)
+            self.truncated.append("<%s%s%s>" % (tag,
+                                                self._flatten_attrs(attrs),
+                                                single_marker))
+
+    def handle_endtag(self, tag):
+        if not self.done:
+            if not tag in self.singles:
+                self.truncated.append("</%s>" % tag)
+            self.queue.pop()
+
+    def handle_data(self, data):
+        self.count += len(data)
+
+        if self.count > self.limit:
+            self.done = True
+            data = data[:(self.count - self.limit)]
+
+        self.truncated.append(data)
+
+    def truncate(self, text):
+
+        self.feed(text)
+
+        while self.queue:
+            self.truncated.append("</%s>" % self.queue.pop())
+
+        return "".join(self.truncated)
