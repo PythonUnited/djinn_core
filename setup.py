f8c7040d8e3de34f5f8e5366524d08d725d68d4b
Martin Bouma <bouma@egoactive.com>
2015-08-31 14:03:34 +0200

    preparing version 1.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_core',
-      version="1.2.1",
+      version="1.2.2",
       description='Djinn Intranet core',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
