3440115b4c14e8907449a9e13c767399a41f64e4
Martin Bouma <bouma@egoactive.com>
2015-09-29 11:06:11 +0200

    pypi upload of 1.2.3 failed

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_core',
-      version="1.2.3",
+      version="1.2.4",
       description='Djinn Intranet core',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
