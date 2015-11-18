8416e071041d17c44b259dfbda2464e44f38f76d
Martin Bouma <bouma@egoactive.com>
2015-11-18 22:06:31 +0100

    pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_core',
-      version="1.2.7",
+      version="1.2.8",
       description='Djinn Intranet core',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
